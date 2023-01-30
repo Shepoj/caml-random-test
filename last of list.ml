7cd4669e1fb42c0cf87ba4499ef2a3eb502e5a93
Joseph "Shepoj" Mdrs <72415872+Shepoj@users.noreply.github.com>
2023-01-30 17:39:58 +0100

    on regle pour que nico arrete de pleurer en vocal !!
    arrete tes sottise le marseillais

--- last of list.ml
+++ last of list.ml
@@ -3,12 +3,15 @@ let rec print_list = function
   | e::l -> print_int e ; print_string " " ; print_list l
 
 let rec last list= 
-  if List.length list==1 then print_list(list) else last (List.tl list);; 
+  if List.length list == 0 then print_string "non" else if List.length list==1 then print_list(list) else last (List.tl list);; 
 
 let rec lasttwo list= 
-  if List.length list==2 then print_list(list) else lasttwo (List.tl list);; 
+  if List.length list <= 1 then print_string "non" else if List.length list==2 then print_list(list) else lasttwo (List.tl list);; 
 
 let liste = [1;2;3;4;5;6];;
+let listevide = [];;
 (*let liste2 = 2::liste;;*)
 last liste;;
-lasttwo liste 
+lasttwo liste;;
+last listevide;;
+lasttwo listevide
